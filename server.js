a6a10b25b7d481e3d524ce19e4722994696cfd2d
theshook <hjefernandez@gmail.com>
2018-08-22 01:43:20 +0800

    Openshift

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-let port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+let port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 app.set('port', port);
