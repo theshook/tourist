3a825fdf23f244a34d72f13248edcd8914d6001b
theshook <hjefernandez@gmail.com>
2018-08-22 12:15:04 +0800

    Heroku

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-let port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+let port = process.env.PORT || 8080;
 
 app.set('port', port);
 const server = http.createServer(app);
@@ -17,6 +16,6 @@ module.exports.io = io;
 // });
 
 
-server.listen(port, ipaddress, (err, res) => {
-  console.log(`Listening on ${ipaddress}, port ${port}`)
+server.listen(port, (err, res) => {
+  console.log(`Listening on port ${port}`)
 });
