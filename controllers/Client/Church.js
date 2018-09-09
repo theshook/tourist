417ec1d66f2370aa1a4cd7f19a419f600f5052ec
theshook <hjefernandez@gmail.com>
2018-09-09 22:46:09 +0800

    Routes

--- controllers/Client/Church.js
+++ controllers/Client/Church.js
@@ -1,29 +1,32 @@
 const db = require("../../db.js");
-let { estab_get_all_query, estab_get_single } = require('./Helpers/QueryHelpers');
+let {
+  estab_get_all_query,
+  estab_get_single
+} = require("./Helpers/QueryHelpers");
 
 exports.get_all_Church = (req, res) => {
-  db.query(
-    estab_get_all_query(3),
-    (err, rows) => {
-      if (err) {
-        throw err;
-      }
-      res.render("Client/Church", {
-        rows: rows,
-        pageTitle: "Churches in Abra"
-      });
+  db.query(estab_get_all_query(3), (err, rows) => {
+    if (err) {
+      throw err;
     }
-  );
+    res.render("Client/Church", {
+      rows: rows,
+      pageTitle: "Churches in Abra",
+      route: "church"
+    });
+  });
 };
 
 exports.church_View = (req, res) => {
   let id = req.params.church_id;
-  db.query(
-    estab_get_single(id),
-    (err, rows) => {
-      if (err) {
-        throw err;
-      }
-      res.render('Client/Church/view', { rows: rows, pageTitle: 'Church Information' });
+  db.query(estab_get_single(id), (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.render("Client/Church/view", {
+      rows: rows,
+      pageTitle: "Church Information",
+      route: "church"
     });
+  });
 };
--- controllers/Client/Hotel.js
+++ controllers/Client/Hotel.js
@@ -1,29 +1,32 @@
 const db = require("../../db.js");
-let { estab_get_all_query, estab_get_single } = require('./Helpers/QueryHelpers');
+let {
+  estab_get_all_query,
+  estab_get_single
+} = require("./Helpers/QueryHelpers");
 
 exports.get_all_Hotel = (req, res) => {
-  db.query(
-    estab_get_all_query(2),
-    (err, rows) => {
-      if (err) {
-        throw err;
-      }
-      res.render(`Client/Hotel`, {
-        rows: rows,
-        pageTitle: `Hotel in Abra`
-      });
+  db.query(estab_get_all_query(2), (err, rows) => {
+    if (err) {
+      throw err;
     }
-  );
+    res.render(`Client/Hotel`, {
+      rows: rows,
+      pageTitle: `Hotel in Abra`,
+      route: "hotel"
+    });
+  });
 };
 
 exports.hotel_View = (req, res) => {
   let id = req.params.hotel_id;
-  db.query(
-    estab_get_single(id),
-    (err, rows) => {
-      if (err) {
-        throw err;
-      }
-      res.render('Client/Hotel/view', { rows: rows, pageTitle: 'Hotel Information' });
+  db.query(estab_get_single(id), (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.render("Client/Hotel/view", {
+      rows: rows,
+      pageTitle: "Hotel Information",
+      route: "hotel"
     });
+  });
 };
