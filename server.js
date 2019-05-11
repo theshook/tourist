72ce617fcf130ca295277b725102d27406a07e9b
theshook <hjefernandez@gmail.com>
2019-05-11 15:29:30 +0800

    asd

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = require('./app');
 
 let port = process.env.PORT || 8080;
 
-app.set('port', port);
 const server = http.createServer(app);
 
-server.listen(port, (err, res) => {
-  console.log(`Listening on port ${port}`)
+server.listen(port, function () {
+  console.log("The YelpCamp Server Has Started!");
 });
+
+//server.listen();
+
